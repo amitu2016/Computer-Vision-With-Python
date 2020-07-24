3c7ea6a9582e147ad7813e140ccde79823a5ad12
Amit Upadhyay <amitu2016@gmail.com>
2020-07-24 21:20:54 +0530

    First opencv app

--- first_opencv.py
+++ first_opencv.py
@@ -1,9 +1,14 @@
 import cv2
 import numpy
 
+# colour, greyscale, unchanged ----> 1, 0, -1
 
-myimage = cv2.imread('DeepLearning.jpeg')
+myimage = cv2.imread('DeepLearning.jpeg',0)
+myimage2 = cv2.imread('DeepLearning.jpeg',-1)
 cv2.imshow("image window",myimage)
+cv2.imshow("image window2",myimage2)
+cv2.imwrite('DL2.jpeg',myimage)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
